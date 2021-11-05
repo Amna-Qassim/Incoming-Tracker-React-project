7370692ea955b01ea0c69e9f4f188948b08ce8f1
Amna Kassim <84710020+Amna-Qassim@users.noreply.github.com>
2021-11-05 15:07:41 +0300

    initial commit

---
+++ src/Component/IncomeItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function IncomeItem({ income, index, removeIncome }) {
+
+  let date = new Date(income.date);
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  const removeHandel = i => {
+    removeIncome(i);
+  }
+  return (
+    <div className="income-item">
+      <button className="remove-item" onClick={ () => removeHandel(index)} > x </button>
+      <div className="desc">{income.desc}</div>
+      <div className="price">£{income.price}</div>
+      <div className="date">{ day + "/" + month + "/" + year }</div>
+
+    </div>
+  )
+}
+
+export default IncomeItem;
